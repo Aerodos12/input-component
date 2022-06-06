4b524082ecda0cee60ae18375e23e15719a9a4e4
Aerodos12 <weldon.henson97@gmail.com>
2022-06-06 17:00:50 -0400

    Fixups + Gamepad calibrarion + Gamepad polling.

--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,150 @@
-import Signsl from "@rbxts/signal"
+import Signal from "@rbxts/signal";
 
 const UIS: UserInputService = game.GetService("UserInputService");
 
 namespace InputComponent {
-	export type MouseType = (Enum.UserInputType.MouseButton1 | Enum.UserInputType.MouseButton2 | Enum.UserInputType.MouseButton3);
-	export const GamepadButtonChanged: Signsl<(button: Enum.KeyCode, isDown: Boolean) => void> = new Signsl<(button: Enum.KeyCode, isDown: Boolean) => void>();
-	export const KeyChanged: Signsl<(key: Enum.KeyCode, isDown: Boolean) => void> = new Signsl<(key: Enum.KeyCode, isDown: Boolean) => void>();
-	export const MouseButtonChanged: Signsl<(key: MouseType, isDown: Boolean) => void> = new Signsl<(key: MouseType, isDown: Boolean) => void>();
+	export type MouseType =
+		| Enum.UserInputType.MouseButton1
+		| Enum.UserInputType.MouseButton2
+		| Enum.UserInputType.MouseButton3;
+	export const GamepadButtonChanged: Signal<(button: Enum.KeyCode, isDown: Boolean) => void> = new Signal<
+		(button: Enum.KeyCode, isDown: Boolean) => void
+	>();
+	export const KeyChanged: Signal<(key: Enum.KeyCode, isDown: Boolean) => void> = new Signal<
+		(key: Enum.KeyCode, isDown: Boolean) => void
+	>();
+	export const MouseButtonChanged: Signal<(key: MouseType, isDown: Boolean) => void> = new Signal<
+		(key: MouseType, isDown: Boolean) => void
+	>();
 
 	export type Platform = "Gamepad" | "Keyboard" | "Touch" | "VR";
-	export const PlatformChanged: Signsl = new Signsl<(platform: Platform) => void>();
-	export let DeviceCount: Number = 0;
+	export let CurrentPlatform: Platform = "Keyboard";
+	export const PlatformChanged: Signal = new Signal<(platform: Platform) => void>();
+	export const DeviceCount: Number = 0;
 	export type DeviceSelector = () => Boolean;
 	export type DeviceSelectorMap = Map<Platform, DeviceSelector>;
 	export const DeviceSelectors: DeviceSelectorMap = new Map<Platform, DeviceSelector>();
-	export function AddDeviceSelector(platform: Platform, selector: DeviceSelector){
+	export function AddDeviceSelector(platform: Platform, selector: DeviceSelector) {
 		DeviceSelectors.set(platform, selector);
 	}
-	export type InputKind = (Enum.KeyCode | MouseType);
+	export type InputKind = Enum.KeyCode | MouseType;
 	type PressedState = Map<InputKind, Boolean>;
 	class PressedHandler {
 		private state: PressedState = new Map<InputKind, Boolean>();
-		public setPressed(key: InputKind, isPressed: Boolean){
+		public setPressed(key: InputKind, isPressed: Boolean) {
 			if (key.EnumType === Enum.UserInputType) {
 				this.state.set(key, isPressed);
 				MouseButtonChanged.Fire(key as MouseType, isPressed);
 				return;
 			}
-			if(UIS.GamepadSupports(Enum.UserInputType.Gamepad1, key as Enum.KeyCode)){
-				GamepadButtonChanged.Fire(key as Enum.KeyCode, isPressed)
-			}	else {
+			if (UIS.GamepadSupports(Enum.UserInputType.Gamepad1, key as Enum.KeyCode)) {
+				GamepadButtonChanged.Fire(key as Enum.KeyCode, isPressed);
+			} else {
 				KeyChanged.Fire(key as Enum.KeyCode, isPressed);
 			}
 			this.state.set(key, isPressed);
 		}
-		public getPressed(key: Enum.KeyCode) : Boolean {
+		public getPressed(key: Enum.KeyCode): Boolean {
 			return this.state.get(key) as Boolean;
 		}
 	}
 	export const Pressed: PressedHandler = new PressedHandler();
 	{
-		Enum.KeyCode.GetEnumItems().forEach(function(keyCode: Enum.KeyCode){
+		Enum.KeyCode.GetEnumItems().forEach(function (keyCode: Enum.KeyCode) {
 			Pressed.setPressed(keyCode as InputKind, false);
-		})
+		});
 	}
 	{
-		Enum.UserInputType.GetEnumItems().forEach(function(mouseButton: Enum.UserInputType){
+		Enum.UserInputType.GetEnumItems().forEach(function (mouseButton: Enum.UserInputType) {
 			const mouseType = mouseButton as MouseType;
-			if (mouseType === undefined){
+			if (mouseType === undefined) {
 				return;
 			}
-			if(!(mouseType.Name.find("MouseButton")[0])){
+			if (mouseType.Name.find("MouseButton")[0] === undefined) {
 				return;
 			}
 			Pressed.setPressed(mouseType as InputKind, false);
-		})
+		});
 	}
-	export type GamepadType = (Enum.UserInputType.Gamepad1 | Enum.UserInputType.Gamepad2 | Enum.UserInputType.Gamepad3 | Enum.UserInputType.Gamepad4 | Enum.UserInputType.Gamepad5 | Enum.UserInputType.Gamepad6 | Enum.UserInputType.Gamepad7 | Enum.UserInputType.Gamepad8 | Enum.UserInputType.None);
-	let ActiveGamepad: (GamepadType | undefined) = undefined;
-	export let CurrentIScheme: string = "General";
+	export type GamepadType =
+		| Enum.UserInputType.Gamepad1
+		| Enum.UserInputType.Gamepad2
+		| Enum.UserInputType.Gamepad3
+		| Enum.UserInputType.Gamepad4
+		| Enum.UserInputType.Gamepad5
+		| Enum.UserInputType.Gamepad6
+		| Enum.UserInputType.Gamepad7
+		| Enum.UserInputType.Gamepad8
+		| Enum.UserInputType.None;
+	let ActiveGamepad: GamepadType | undefined = undefined;
+	export const CurrentIScheme = "General";
 	export interface SensitivityOptions {
 		mouse: number;
 		aim: number;
 		touch: Vector2;
-	};
-	export let Sensitivity: SensitivityOptions = {
+	}
+	export const Sensitivity: SensitivityOptions = {
 		mouse: 0.3,
 		aim: 0.2,
-		touch: new Vector2(math.pi * 2.25, math.pi * 2)
+		touch: new Vector2(math.pi * 2.25, math.pi * 2),
 	};
-	export function GetActiveGamepad() : (GamepadType | undefined) {
-		let activateGamepad: (GamepadType | undefined) = undefined;
-		let navigationGamepads: Array<GamepadType> = new Array<GamepadType>();
-		let navGamepads: Array<Enum.UserInputType> = UIS.GetNavigationGamepads();
+	export function GetActiveGamepad(): GamepadType | undefined {
+		const navGamepads: Array<Enum.UserInputType> = UIS.GetNavigationGamepads();
 		if (navGamepads.size() > 0) {
-			navGamepads.forEach(function(gamepad, index){
-				if (ActiveGamepad === undefined){
+			navGamepads.forEach(function (gamepad, index) {
+				if (ActiveGamepad === undefined) {
 					ActiveGamepad = gamepad as GamepadType;
-				} else if ((ActiveGamepad !== undefined) && navGamepads[index].Value > ActiveGamepad.Value){
+				} else if (ActiveGamepad !== undefined && navGamepads[index].Value > ActiveGamepad.Value) {
 					ActiveGamepad = gamepad as GamepadType;
 				}
-			});	
-
+			});
 		} else {
-			let connGamapads: Array<Enum.UserInputType> = UIS.GetConnectedGamepads();
+			const connGamapads: Array<Enum.UserInputType> = UIS.GetConnectedGamepads();
 			if (connGamapads.size() > 0) {
-				connGamapads.forEach(function(gamepad, index){
-					if (ActiveGamepad === undefined){
+				connGamapads.forEach(function (gamepad, index) {
+					if (ActiveGamepad === undefined) {
 						ActiveGamepad = gamepad as GamepadType;
-					} else if ((ActiveGamepad !== undefined) && connGamapads[index].Value > ActiveGamepad.Value){
+					} else if (ActiveGamepad !== undefined && connGamapads[index].Value > ActiveGamepad.Value) {
 						ActiveGamepad = gamepad as GamepadType;
 					}
-				});	
+				});
 			}
-			if (ActiveGamepad === undefined){
+			if (ActiveGamepad === undefined) {
 				ActiveGamepad = Enum.UserInputType.None;
 			}
 		}
-		return (ActiveGamepad !== Enum.UserInputType.None) ? ActiveGamepad : undefined;
+		return ActiveGamepad !== Enum.UserInputType.None ? ActiveGamepad : undefined;
 	}
-	
+	export type PolledGamepadMap = Map<InputKind, InputObject>;
+	const gamepadPollMap: PolledGamepadMap = new Map<InputKind, InputObject>();
+	let regularKey: InputKind = UIS.GetConnectedGamepads().size() > 0 ? Enum.KeyCode.ButtonR1 : Enum.KeyCode.I;
+	export function refreshPolledStates(gp: GamepadType) {
+		const gpStates = UIS.GetGamepadState(gp as Enum.UserInputType);
+		gpStates.forEach(function (state: InputObject) {
+			gamepadPollMap.set(state.KeyCode as InputKind, state);
+		});
+	}
+	{
+		if (UIS.GamepadEnabled) {
+			ActiveGamepad = GetActiveGamepad();
+			UIS.GamepadConnected.Connect(function (gamepad: Enum.UserInputType) {
+				print(gamepad.Name + " is connected.");
+				regularKey = UIS.GetConnectedGamepads().size() > 0 ? Enum.KeyCode.ButtonR1 : Enum.KeyCode.I;
+				ActiveGamepad = GetActiveGamepad();
+				refreshPolledStates(ActiveGamepad as GamepadType);
+			});
+			UIS.GamepadDisconnected.Connect(function (gamepad: Enum.UserInputType) {
+				print(gamepad.Name + " is disconnected.");
+				regularKey = UIS.GetConnectedGamepads().size() > 0 ? Enum.KeyCode.ButtonR1 : Enum.KeyCode.I;
+				ActiveGamepad = GetActiveGamepad();
+				refreshPolledStates(ActiveGamepad as GamepadType);
+			});
+			if (UIS.GetConnectedGamepads().size() > 0) {
+				CurrentPlatform = "Gamepad";
+				refreshPolledStates(ActiveGamepad as GamepadType);
+			}
+		}
+	}
 }
 
-export = InputComponent
+export = InputComponent;
