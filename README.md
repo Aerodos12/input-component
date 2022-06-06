563878aa3901d17f707365b77d9788507f5cebad
Aerodos12 <weldon.henson97@gmail.com>
2022-06-06 19:30:56 -0400

    Refactoring + platform counter

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # InputComponent
+
 ![RW InputComponent logo](assets/RWIC.png)
 
 Rodblogan Warfare's InputComponent Library realized in TypeScript.
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+/**
+ * A library for creating user interactions via user input.
+ *
+ * @package
+ */
 import Signal from "@rbxts/signal";
 
 const UIS: UserInputService = game.GetService("UserInputService");
@@ -7,31 +12,49 @@ namespace InputComponent {
 		| Enum.UserInputType.MouseButton1
 		| Enum.UserInputType.MouseButton2
 		| Enum.UserInputType.MouseButton3;
-	export const GamepadButtonChanged: Signal<(button: Enum.KeyCode, isDown: Boolean) => void> = new Signal<
-		(button: Enum.KeyCode, isDown: Boolean) => void
+	/**
+	 * Event that fires when a gamepad button is pressed or released.
+	 * @public
+	 */
+	export const GamepadButtonChanged: Signal<(button: Enum.KeyCode, isDown: boolean) => void> = new Signal<
+		(button: Enum.KeyCode, isDown: boolean) => void
 	>();
-	export const KeyChanged: Signal<(key: Enum.KeyCode, isDown: Boolean) => void> = new Signal<
-		(key: Enum.KeyCode, isDown: Boolean) => void
+	/**
+	 * Event that fires when a key is pressed or released.
+	 * @public
+	 */
+	export const KeyChanged: Signal<(key: Enum.KeyCode, isDown: boolean) => void> = new Signal<
+		(key: Enum.KeyCode, isDown: boolean) => void
 	>();
-	export const MouseButtonChanged: Signal<(key: MouseType, isDown: Boolean) => void> = new Signal<
+	/**
+	 * Event that fires when a mouse button is pressed or released.
+	 * @public
+	 */
+	export const MouseButtonChanged: Signal<(key: MouseType, isDown: boolean) => void> = new Signal<
 		(key: MouseType, isDown: Boolean) => void
 	>();
 
 	export type Platform = "Gamepad" | "Keyboard" | "Touch" | "VR";
 	export let CurrentPlatform: Platform = "Keyboard";
 	export const PlatformChanged: Signal = new Signal<(platform: Platform) => void>();
-	export const DeviceCount: Number = 0;
-	export type DeviceSelector = () => Boolean;
+	export let DeviceCount = 0;
+	export type DeviceSelector = () => boolean;
 	export type DeviceSelectorMap = Map<Platform, DeviceSelector>;
 	export const DeviceSelectors: DeviceSelectorMap = new Map<Platform, DeviceSelector>();
+	/**
+	 * Adds a function to a map of them that determines the value of {@link CurrentPlatform}.
+	 *
+	 * @param platform The platform that corresponds to this selector
+	 * @param selector The actual function that determines how {@link CurrentPlatform} changes.
+	 */
 	export function AddDeviceSelector(platform: Platform, selector: DeviceSelector) {
 		DeviceSelectors.set(platform, selector);
 	}
 	export type InputKind = Enum.KeyCode | MouseType;
-	type PressedState = Map<InputKind, Boolean>;
+	type PressedState = Map<InputKind, boolean>;
 	class PressedHandler {
-		private state: PressedState = new Map<InputKind, Boolean>();
-		public setPressed(key: InputKind, isPressed: Boolean) {
+		private state: PressedState = new Map<InputKind, boolean>();
+		public setPressed(key: InputKind, isPressed: boolean) {
 			if (key.EnumType === Enum.UserInputType) {
 				this.state.set(key, isPressed);
 				MouseButtonChanged.Fire(key as MouseType, isPressed);
@@ -44,8 +67,8 @@ namespace InputComponent {
 			}
 			this.state.set(key, isPressed);
 		}
-		public getPressed(key: Enum.KeyCode): Boolean {
-			return this.state.get(key) as Boolean;
+		public getPressed(key: Enum.KeyCode): boolean {
+			return this.state.get(key) as boolean;
 		}
 	}
 	export const Pressed: PressedHandler = new PressedHandler();
@@ -66,6 +89,9 @@ namespace InputComponent {
 			Pressed.setPressed(mouseType as InputKind, false);
 		});
 	}
+	/**
+	 * Denotes one of the 8 Gamepads that can be connected at once to Roblox.
+	 */
 	export type GamepadType =
 		| Enum.UserInputType.Gamepad1
 		| Enum.UserInputType.Gamepad2
@@ -77,7 +103,16 @@ namespace InputComponent {
 		| Enum.UserInputType.Gamepad8
 		| Enum.UserInputType.None;
 	let ActiveGamepad: GamepadType | undefined = undefined;
+	/**
+	 * Determines the current scope of input (how inputs are handled within this library).
+	 * @public
+	 */
 	export const CurrentIScheme = "General";
+	/**
+	 * An interface used for configuring mouse sensitivity globally in Rodblogan Warfare.
+	 * @interface
+	 * @public
+	 */
 	export interface SensitivityOptions {
 		mouse: number;
 		aim: number;
@@ -88,6 +123,11 @@ namespace InputComponent {
 		aim: 0.2,
 		touch: new Vector2(math.pi * 2.25, math.pi * 2),
 	};
+	/**
+	 * Retrieves the currently active gamepad.
+	 *
+	 * @returns The current gamepad controller in use as a {@link GamepadType}
+	 */
 	export function GetActiveGamepad(): GamepadType | undefined {
 		const navGamepads: Array<Enum.UserInputType> = UIS.GetNavigationGamepads();
 		if (navGamepads.size() > 0) {
@@ -115,9 +155,18 @@ namespace InputComponent {
 		}
 		return ActiveGamepad !== Enum.UserInputType.None ? ActiveGamepad : undefined;
 	}
+	/**
+	 * Map used for polled inputs on console.
+	 */
 	export type PolledGamepadMap = Map<InputKind, InputObject>;
 	const gamepadPollMap: PolledGamepadMap = new Map<InputKind, InputObject>();
 	let regularKey: InputKind = UIS.GetConnectedGamepads().size() > 0 ? Enum.KeyCode.ButtonR1 : Enum.KeyCode.I;
+	/**
+	 * Updates the polled inputs for the currently active gamepad controller.
+	 *
+	 * @param gp The gamepad to get polled {@link InputObject}s from
+	 * @internal
+	 */
 	export function refreshPolledStates(gp: GamepadType) {
 		const gpStates = UIS.GetGamepadState(gp as Enum.UserInputType);
 		gpStates.forEach(function (state: InputObject) {
@@ -148,6 +197,9 @@ namespace InputComponent {
 			CurrentPlatform = "Touch";
 		}
 	}
+	/**
+	 * Refreshes the {@link CurrentPlatform} property.
+	 */
 	export function RecalibratePlaform() {
 		const lastType = UIS.GetLastInputType();
 		if (lastType === Enum.UserInputType.Keyboard || lastType.Name.find("Mouse")[0] !== undefined) {
@@ -161,6 +213,11 @@ namespace InputComponent {
 		}
 	}
 
+	/**
+	 * Retrieves the current platform based on the last known input type used.
+	 *
+	 * @returns The platform to use for input-related tasks.
+	 */
 	export function GetPlatform(): Platform {
 		const lastType = UIS.GetLastInputType();
 		if (lastType === Enum.UserInputType.Keyboard || lastType.Name.find("Mouse")[0] !== undefined) {
@@ -172,6 +229,19 @@ namespace InputComponent {
 		} else {
 			return "VR";
 		}
+	}
+
+	/**
+	 * Calculates how many platforms are usable.
+	 */
+	export function EvaluatePlatformCount() {
+		DeviceCount = 0;
+		DeviceSelectors.forEach(function (f: DeviceSelector) {
+			const result = f();
+			if (result) {
+				DeviceCount++;
+			}
+		});
 	}
 }
 
