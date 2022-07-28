307628606f5c6b92dcccb2e2e154eb67fe03ab65
Aerodos12 <weldon.henson97@gmail.com>
2022-07-28 16:17:30 -0400

    Added DATA_FORMATS map

--- src/subcomponents/inputAxis.ts
+++ src/subcomponents/inputAxis.ts
@@ -25,8 +25,35 @@ namespace InputAxisSC {
 	 * Used for rotary inputs.
 	 */
 	export enum AxisRotationMode {
+		/**
+		 * Handle rotary values with your own way (function)
+		 */
 		Custom,
+		/**
+		 * Handle rotary inputs the usual way (Rodblogan Warfare style)
+		 */
 		Conventional,
+	}
+
+	export type DataFormatMap = Map<Enum.UserInputType, AxisDataFormat>;
+
+	export const DATA_FORMATS: DataFormatMap = new Map<Enum.UserInputType, AxisDataFormat>();
+
+	{
+		DATA_FORMATS.set(Enum.UserInputType.MouseMovement, AxisDataFormat.Position);
+		DATA_FORMATS.set(Enum.UserInputType.MouseWheel, AxisDataFormat.Position);
+		DATA_FORMATS.set(Enum.UserInputType.Gamepad1, AxisDataFormat.Position);
+		DATA_FORMATS.set(Enum.UserInputType.Gamepad2, AxisDataFormat.Position);
+		DATA_FORMATS.set(Enum.UserInputType.Gamepad3, AxisDataFormat.Position);
+		DATA_FORMATS.set(Enum.UserInputType.Gamepad4, AxisDataFormat.Position);
+		DATA_FORMATS.set(Enum.UserInputType.Gamepad5, AxisDataFormat.Position);
+		DATA_FORMATS.set(Enum.UserInputType.Gamepad6, AxisDataFormat.Position);
+		DATA_FORMATS.set(Enum.UserInputType.Gamepad7, AxisDataFormat.Position);
+		DATA_FORMATS.set(Enum.UserInputType.Gamepad8, AxisDataFormat.Position);
+		DATA_FORMATS.set(Enum.UserInputType.Keyboard, AxisDataFormat.Analog);
+		DATA_FORMATS.set(Enum.UserInputType.MouseButton1, AxisDataFormat.Analog);
+		DATA_FORMATS.set(Enum.UserInputType.MouseButton2, AxisDataFormat.Analog);
+		DATA_FORMATS.set(Enum.UserInputType.MouseButton3, AxisDataFormat.Analog);
 	}
 }
 
